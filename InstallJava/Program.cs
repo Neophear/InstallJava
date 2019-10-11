cb281d76613e415edb68a4c76cff8c26ffd91432
Neophear <stiiggade@gmail.com>
2019-10-11 10:07:26 +0200

    Initial commit

---
+++ InstallJava/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstallJava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Security.SecureString password = new System.Security.SecureString();
+            foreach (char character in "d12345**")
+            {
+                password.AppendChar(character);
+            }
+
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                WorkingDirectory = "\\\\10.10.10.10\\Java_til_Bruger\\",
+                FileName = "calc.exe",
+                UserName = "diablo",
+                Password = password,
+                Domain = "INTERNET.LOCAL",
+                UseShellExecute = false
+            };
+
+            System.Diagnostics.Process.Start(startInfo);
+        }
+    }
+}
